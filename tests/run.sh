9e748236b87894d76bca0c1850808a428bdb7614
Valentin Lorentz <progval@gmail.com>
2013-12-27 13:37:28 +0100

    Add tests.

---
+++ tests/run.sh
@@ -0,0 +1,14 @@
+rm -f _build/test_output.txt
+
+for filename in $(ls tests/*.imp)
+do
+    echo "
+$filename
+===">> _build/test_output.txt
+    cat $filename | ./impterpreter >> _build/test_output.txt
+done
+
+diff -u _build/test_output.txt tests/expected_output.txt > _build/tests.diff
+RETVAL=$?
+[ $RETVAL -eq 0 ] && echo "Succès"
+[ $RETVAL -ne 0 ] && echo "Échec (voir _build/tests.diff)"
